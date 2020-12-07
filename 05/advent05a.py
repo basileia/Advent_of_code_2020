c74d9722414c51164c4d009d933247a309db81b9
basileia <jane.novak@centrum.cz>
2020-12-07 13:41:10 +0100

    Day 5 part 1

---
+++ 05/advent05a.py
@@ -0,0 +1,46 @@
+with open("input.txt", encoding="utf-8") as file:
+    board_passes = [line.strip() for line in file]
+
+
+def get_row(board_pass, number, number_of_rows):
+    board_row = board_pass[:number]
+    interval = [0, number_of_rows - 1]
+
+    for char in board_row:
+        if char == "F":
+            interval[1] -= number_of_rows / 2
+            number_of_rows = number_of_rows / 2
+        elif char == "B":
+            interval[0] += number_of_rows / 2
+            number_of_rows = number_of_rows / 2
+
+    if interval[0] == interval[1]:
+        return interval[0]
+    else:
+        return None
+
+
+def get_column(board_pass, number, number_of_cols):
+    board_col = board_pass[number:]
+    interval = [0, number_of_cols - 1]
+
+    for char in board_col:
+        if char == "L":
+            interval[1] -= number_of_cols / 2
+            number_of_cols = number_of_cols / 2
+        elif char == "R":
+            interval[0] += number_of_cols / 2
+            number_of_cols = number_of_cols / 2
+
+    if interval[0] == interval[1]:
+        return interval[0]
+    else:
+        return None
+
+
+seat_ids = []
+for board_pass in board_passes:
+    id = get_row(board_pass, 7, 128) * 8 + get_column(board_pass, 7, 8)
+    seat_ids.append(id)
+
+print(max(seat_ids))
