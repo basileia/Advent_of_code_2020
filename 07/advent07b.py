b564fda1c6c176524c8cf780fac05ff31ee4893b
basileia <jane.novak@centrum.cz>
2020-12-08 20:17:59 +0100

    Day 7 part 2

--- 07/advent07b.py
+++ 07/advent07b.py
@@ -1,41 +1,27 @@
-import re
-
-with open("sample.txt", encoding="utf-8") as file:
+with open("input.txt", encoding="utf-8") as file:
     rules = file.readlines()
 
 result = {}
 for item in rules:
-    key, val = item.strip().replace(".", "").split(" contain", 1)
+    key, val = item.strip().replace(".", "").split(" contain ", 1)
     result[key] = val.split(", ")
 
 
-values_bags = []
-
-
-def get_sum(search_value, number_of_bags, sum):
-    wanted = result[search_value]
-    for i in wanted:
-        try:
-            num = int(re.match(r"\d", i.strip()).group())
-            values_bags.append(i)
-            sum += number_of_bags * num
-        except AttributeError:
-            values_bags.append(i)
+def get_sum(curr_bag):
+    sum = 0
+    children = []
+    for bag in result:
+        if curr_bag == bag:
+            children = [item for item in result[bag]]
+    print(children)
+    for child in children:
+        if child == "no other bags":
+            continue
+        if child:
+            if child[2:][-1] != "s":
+                child = child + "s"
+            sum += int(child[0]) + int(child[0]) * get_sum(child[2:])
     return sum
 
 
-values_bags.append("shiny gold bags")
-sum = get_sum("shiny gold bags", 1, 0)
-num = 1
-
-for values_bag in values_bags:
-    value = re.search(r"([a-z]+([a-z]| ))+", values_bag.lstrip()).group()
-    if value[-1] != "s":
-        value = value + "s"
-    if value == "no other bags":
-        continue
-    if value == "shiny gold bags":
-        continue
-    num = int(re.match(r"\d", values_bag.lstrip()).group()) * num
-    sum = get_sum(value, num, sum)
-print(sum)
+print(get_sum("shiny gold bags"))
